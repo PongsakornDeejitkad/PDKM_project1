b9b6718b7dd639b81bf7c8f80eed2462168139b5
Pongsakorn <ohm_pongsakorn2@gmail.com>
2024-02-20 09:56:58 +0700

    adjust custumer list

--- models/customer.go
+++ models/customer.go
@@ -12,33 +12,33 @@ type Customer struct {
 	UpdatedAt time.Time `json:"updated_at" gorm:"default: NOW()"`
 
 	CustomerPayments []CustomerPayment `json:"customer_payments" gorm:"foreignKey:CustomerID; cascade; constraint:OnUpdate:CASCADE,OnDelete:CASCADE"`
-	CustomerAddress []CustomerAddress `json:"customer_address" gorm:"foreignKey:CustomerID; cascade; constraint:OnUpdate:CASCADE,OnDelete:CASCADE"`
+	CustomerAddress  []CustomerAddress `json:"customer_address" gorm:"foreignKey:CustomerID; cascade; constraint:OnUpdate:CASCADE,OnDelete:CASCADE"`
 }
 
 type CustomerPayment struct {
 	ID          int       `json:"id" gorm:"primary_key"`
-	CustomerID  int       `json:"customer_id"`
-	PaymentType string    `json:"payment_type"`
-	Provider    string    `json:"provider"`
-	AccountNo   string    `json:"account_no"`
-	ExpireDate  time.Time `json:"expire_date"`
-	CreatedAt   time.Time `json:"created_at"`
-	UpdatedAt   time.Time `json:"updated_at"`
+	CustomerID  int       `json:"customer_id" gorm:"interger"`
+	PaymentType string    `json:"payment_type" gorm:"varchar(100)"`
+	Provider    string    `json:"provider" gorm:"varchar(100)"`
+	AccountNo   int       `json:"account_no" gorm:"integer"`
+	ExpireDate  time.Time `json:"expire_date" gorm:"integer"`
+	CreatedAt   time.Time `json:"created_at" gorm:"default: NOW()"`
+	UpdatedAt   time.Time `json:"updated_at" gorm:"default: NOW()"`
 
 	Customer Customer `json:"customer" gorm:"foreignKey:CustomerID"`
 }
 
 type CustomerAddress struct {
-	ID          int    `json:"id" gorm:"primary_key"`
-	CustomerID  int    `json:"customer_id"`
-	District    string `json:"district"`
-	SubDistrict string `json:"sub_district"`
-	City        string `json:"city"`
-	ZipCode     string `json:"zip_code"`
-	Country     string `json:"country"`
-	Telephone   string `json:"telephone"`
-	CreatedAt   time.Time `json:"created_at"`
-	UpdatedAt   time.Time `json:"updated_at"`
+	ID          int       `json:"id" gorm:"primary_key"`
+	CustomerID  int       `json:"customer_id" gorm:"interger"`
+	District    string    `json:"district" gorm:"varchar(100)"`
+	SubDistrict string    `json:"sub_district" gorm:"varchar(100)"`
+	City        string    `json:"city" gorm:"varchar(100)"`
+	ZipCode     string    `json:"zip_code" gorm:"varchar(100)"`
+	Country     string    `json:"country" gorm:"varchar(100)"`
+	Telephone   int       `json:"telephone" gorm:"integer"`
+	CreatedAt   time.Time `json:"created_at" gorm:"default: NOW()"`
+	UpdatedAt   time.Time `json:"updated_at" gorm:"default: NOW()"`
 
 	Customer Customer `json:"customer" gorm:"foreignKey:CustomerID"`
 }
