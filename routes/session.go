df57658bd9ca825e668645e57346af8ff0e6b3e8
Pongsakorn <ohm_pongsakorn2@gmail.com>
2024-02-24 10:56:42 +0700

    create routes of session feature

--- routes/session.go
+++ routes/session.go
@@ -10,4 +10,7 @@ func SessionRoutes(e *echo.Group) {
 	sessionRoutes := e.Group("/sessions")
 
 	sessionRoutes.GET("", controllers.ListSessions)
+	sessionRoutes.GET("/:id", controllers.GetSession)
+	sessionRoutes.POST("", controllers.CreateSession)
+	sessionRoutes.GET("/:id/cartItem", controllers.GetCartItems)
 }
